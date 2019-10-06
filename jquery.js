9140627d59b067f94d8e7de2410755b4924a8393
makender103 <makender103@gmail.com>
2019-10-06 13:14:22 -0300

    novo  design

---
+++ jquery.js
@@ -0,0 +1,13 @@
+
+    /** $("#teste").hover(function(){
+
+        $("#pteste").html("Hello <b>world</b>!")
+        $("#teste").hide()
+
+    },function(){
+        //$("#pteste").html("Hello <b>world</b>!").remove()
+        $("#teste").show()
+    });
+
+    **/
+    
